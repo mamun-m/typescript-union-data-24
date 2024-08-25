9fd96467a75f67d4a5bf9b0aa93b73043e956471
Mohammad mamun <miam6829@gmail.com>
2024-08-25 20:53:53 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// union data type in typescript
+var userName;
+userName = "Mohammad Mamun khan";
+userName = 545645456;
+userName = typeof true;
+// typescript funtion
+function first(num1, num2) {
+    console.log(num1 + num2);
+}
+first(20, 30);
+var fullNames = "burning boy abir";
+console.log(fullNames);
---
+++ index.ts
@@ -0,0 +1,17 @@
+// union data type in typescript
+
+let userName: string | number | boolean;
+
+userName = "Mohammad Mamun khan";
+userName = 545645456;
+userName = typeof true;
+// typescript funtion
+
+function first(num1, num2): void {
+  console.log(num1 + num2);
+}
+first(20, 30);
+
+const fullNames: string = "burning boy abir";
+
+console.log(fullNames);
